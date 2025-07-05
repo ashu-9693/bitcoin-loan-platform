45ce98b3a4124ffd8411de047848da03cef4e4f1
ashu-9693 <ashutoshkmrthkr@gmail.com>
2025-07-05 04:58:36 +0000

    Initial commit - working bitcoin loan project with frontend + backend

--- src/btc_loan_project_frontend/vite.config.js
+++ src/btc_loan_project_frontend/vite.config.js
@@ -1,42 +1,7 @@
-import { defineConfig } from 'vite';
-import { fileURLToPath, URL } from 'url';
-import environment from 'vite-plugin-environment';
-import dotenv from 'dotenv';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-dotenv.config({ path: '../../.env' });
-
+// https://vite.dev/config/
 export default defineConfig({
-  build: {
-    emptyOutDir: true,
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: "globalThis",
-      },
-    },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://127.0.0.1:4943",
-        changeOrigin: true,
-      },
-    },
-  },
-  publicDir: "assets",
-  plugins: [
-    environment("all", { prefix: "CANISTER_" }),
-    environment("all", { prefix: "DFX_" }),
-  ],
-  resolve: {
-    alias: [
-      {
-        find: "declarations",
-        replacement: fileURLToPath(
-          new URL("../declarations", import.meta.url)
-        ),
-      },
-    ],
-  },
-});
+  plugins: [react()],
+})
